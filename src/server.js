4a13b778d527e3de1da550cb8c7335b3583311b8
msachi <marchee88@gmail.com>
2017-04-02 11:17:48 +0100

    Completed solution

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const hapi = require('hapi');
+const server = new hapi.Server();
+const inert = require('inert');
+
+server.connection({
+  host: 'localhost',
+  port: 3000
+});
+
+server.route({
+  method: 'GET',
+  path: '/hello',
+  handler: (request, reply) => {
+    reply('Hello world!');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/user/{name}',
+  handler: (request, reply) => {
+    reply(`Hello ${encodeURIComponent(request.params.name)}`);
+  }
+});
+
+server.register(inert, err => {
+  if (err) throw err;
+  server.route({
+    method: 'GET',
+    path: '/{file*}',
+    handler: {
+      directory: {
+        path: 'public/'
+      }
+    }
+  });
+});
+
+server.start(err => {
+  if (err) throw err;
+  console.log(`Server running at ${server.info.uri}`);
+})
