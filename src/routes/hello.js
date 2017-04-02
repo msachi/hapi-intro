9836fbae602239cc5ecf57ae06eeb8c29f3ca6bc
msachi <marchee88@gmail.com>
2017-04-02 11:32:02 +0100

    Solve merge conflict

--- src/routes/hello.js
+++ src/routes/hello.js
@@ -2,12 +2,6 @@ module.exports = {
   method: 'GET',
   path: '/hello',
   handler: (request, reply) => {
-<<<<<<< HEAD
     reply('Hello world!');
   }
 };
-=======
-    reply('Hello, world!');
-  }
-}
->>>>>>> e0543a7203d5dbc77b05565e92f886c72161e435
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,5 @@
 module.exports = [
   require('./hello.js'),
-<<<<<<< HEAD
   require('./user.js'),
-=======
-  require('./name.js'),
->>>>>>> e0543a7203d5dbc77b05565e92f886c72161e435
   require('./static.js')
 ];
